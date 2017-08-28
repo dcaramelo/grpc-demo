b58459c1bc7c070af3307ef6e0aa74986b400d50
Carles Sistare <carles.sistare@googlemail.com>
2017-08-28 21:38:47 +0200

    Add live coding template

---
+++ src/node/client-grpc-simple-livecoding.js
@@ -0,0 +1,13 @@
+const PROTO_PATH = process.env.PROTO || '../main/proto/example.proto'
+const HOST = process.env.HOST || 'localhost'
+
+const MESSAGE = {
+  name: 'Peter'
+}
+
+const main = function() {
+
+
+}
+
+main()
--- src/node/client-grpc-simple.js
+++ src/node/client-grpc-simple.js
@@ -1,12 +1,13 @@
 const PROTO_PATH = process.env.PROTO || '../main/proto/example.proto'
 const HOST = process.env.HOST || 'localhost'
 
-const grpc = require('grpc')
-const example_proto = grpc.load(PROTO_PATH).demo.beans.proto
-
 const MESSAGE = {
   name: 'Peter'
 }
+
+const grpc = require('grpc')
+const example_proto = grpc.load(PROTO_PATH).demo.beans.proto
+
 
 const main = function() {
   const client = new example_proto.Example(HOST + ':50052', grpc.credentials.createInsecure())
