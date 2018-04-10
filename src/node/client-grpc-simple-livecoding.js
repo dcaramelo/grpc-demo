f31f81477892b318a0459c35404364be2ab669d3
Carles Sistare <carles.sistare@googlemail.com>
2018-04-10 22:06:17 +0200

    Add port

--- src/node/client-grpc-simple-livecoding.js
+++ src/node/client-grpc-simple-livecoding.js
@@ -1,5 +1,5 @@
 const PROTO_PATH = process.env.PROTO || '../main/proto/example.proto'
-const HOST = process.env.HOST || 'localhost'
+const HOST = process.env.HOST || 'localhost:50052'
 
 const MESSAGE = {
   name: 'Peter'
